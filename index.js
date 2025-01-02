36b422a0d658b1a142589e524537f1ddeb1e57c2
albertcfranca <96530865+albertcfranca@users.noreply.github.com>
2025-01-02 10:58:33 -0300

    configuracoes de nodemon

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 
+/**
+ * GET = BUSCAR INFORMACAO
+ * DELETE = DELETAR UM DADO
+ * PUT = ALTERAR UM DADOS
+ * POST = CRIAR UM DADOS
+ */
+
+// http://localhost:3000/test
+
+app.get('/test',(request, response) => {
+  return response.send('Hello world tchunaite o my goood');
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
